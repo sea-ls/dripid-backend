5646726b55720b1706ea139f066847d3fb609b7f
grihan181 <osagrigoriy@mail.ru>
2024-02-26 20:16:31 +0400

    change dir

--- src/main/java/ru/seals/dripid/DripidApplication.java
+++ src/main/java/ru/seals/dripid/DripidApplication.java
@@ -1,4 +1,4 @@
-package su.seals.dripid;
+package ru.seals.dripid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ru/seals/dripid/DripidApplicationTests.java
+++ src/test/java/ru/seals/dripid/DripidApplicationTests.java
@@ -1,4 +1,4 @@
-package su.seals.dripid;
+package ru.seals.dripid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
