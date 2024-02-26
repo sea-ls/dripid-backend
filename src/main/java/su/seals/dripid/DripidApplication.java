a32ef66454690d650b69e3319d4d983bd141082a
grihan181 <osagrigoriy@mail.ru>
2024-02-26 20:13:58 +0400

    init

---
+++ src/main/java/su/seals/dripid/DripidApplication.java
@@ -0,0 +1,13 @@
+package su.seals.dripid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DripidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/su/seals/dripid/DripidApplicationTests.java
@@ -0,0 +1,13 @@
+package su.seals.dripid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
