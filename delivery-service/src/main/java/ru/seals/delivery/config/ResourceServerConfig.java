a0693bb3c66849fd5d92bba147cf7600b2c2fdfe
McCayl <realcayl.cl@gmail.com>
2024-03-06 22:44:06 +0400

    auth-service: add security

---
+++ delivery-service/src/main/java/ru/seals/delivery/config/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package ru.seals.delivery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class ResourceServerConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/api/v1/admin/enum")
+                .authorizeHttpRequests(authorize -> authorize.anyRequest()
+                        .hasAuthority("SCOPE_resource.read"))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
--- delivery-service/src/main/java/ru/seals/delivery/controller/EnumController.java
+++ delivery-service/src/main/java/ru/seals/delivery/controller/EnumController.java
@@ -1,6 +1,7 @@
 package ru.seals.delivery.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/v1/admin/enum")
 @RequiredArgsConstructor
 public class EnumController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello world";
+    }
     /*@GetMapping("/delivery_status")
     public DeliveryStatus[] getAllDeliveryStatuses() {
         return DeliveryStatus.values();
