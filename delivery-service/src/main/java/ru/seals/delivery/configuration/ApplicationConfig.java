50b651b5f798c5c5619a2c565ade520c5dd2de74
Kirill <dekerilljr@gmail.com>
2024-04-06 18:53:55 +0400

    test (5)

--- delivery-service/src/main/java/ru/seals/delivery/configuration/ApplicationConfig.java
+++ delivery-service/src/main/java/ru/seals/delivery/configuration/ApplicationConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableFeignClients(basePackages = {"ru.seals.delivery.controller.clients"})
 public class ApplicationConfig {
-
     @Bean
     public ModelMapper getMapper() {
         return new ModelMapper();
