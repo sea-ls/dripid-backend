f51cc826adbe9e3862af0561bceceeb19c9d93fc
Kirill <dekerilljr@gmail.com>
2024-04-08 20:09:15 +0400

    test (10)

--- delivery-service/src/main/java/ru/seals/delivery/configuration/SecurityConfig.java
+++ delivery-service/src/main/java/ru/seals/delivery/configuration/SecurityConfig.java
@@ -16,8 +16,6 @@ import ru.seals.delivery.security.JwtAuthConverter;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-
     private final JwtAuthConverter jwtAuthConverter;
     private final AuthenticationEntryPoint authenticationEntryPoint;
 
