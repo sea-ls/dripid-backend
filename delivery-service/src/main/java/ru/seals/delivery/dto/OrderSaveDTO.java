e6fa5d649f25980c8494cbb11d0ee825dcc371e7
Kirill <dekerilljr@gmail.com>
2024-05-02 17:36:44 +0400

    fix view order endpoints in swagger (3)

--- delivery-service/src/main/java/ru/seals/delivery/dto/OrderSaveDTO.java
+++ delivery-service/src/main/java/ru/seals/delivery/dto/OrderSaveDTO.java
@@ -14,15 +14,15 @@ import java.util.List;
 
 @Data
 public class OrderSaveDTO {
-    private OrderType orderType;
+    /*private OrderType orderType;
     private OrderStatus orderStatus;
     private LocalDateTime lastUpdate;
-    //private List<Product> products = new ArrayList<>();
+    private List<Product> products = new ArrayList<>();
     private Warehouse warehouse;
     private DeliveryStageType deliveryStageType;
     private Person person;
     private String deliveryHistory;
     private String trackNumberInternal;
-    private String trackNumberExternal;
+    private String trackNumberExternal;*/
     private String address;
 }
