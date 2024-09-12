3d20b89a77f36413338a4cc4180ea31767925929
McCayl <realcayl.cl@gmail.com>
2024-09-13 00:07:00 +0400

    delivery-service: fixing date issue (9)

--- delivery-service/src/main/java/ru/seals/delivery/configuration/JacksonConfig.java
+++ delivery-service/src/main/java/ru/seals/delivery/configuration/JacksonConfig.java
@@ -1,6 +1,7 @@
 package ru.seals.delivery.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.jackson.datatype.money.MoneyModule;
@@ -11,7 +12,7 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new MoneyModule());
-        //objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
